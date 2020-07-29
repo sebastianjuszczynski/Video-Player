427b3f76f92d157e69d0f9dc82a01f7c47e21a02
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-07-29 21:23:03 +0100

    function handleRangeUpdate added

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
-const ranges = player.querySelector(".player__slider");
+const ranges = player.querySelectorAll(".player__slider");
 
 function togglePlay() {
     if (video.paused) {
@@ -20,9 +20,14 @@ function updateButtonIcon() {
 function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 };
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+};
 
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButtonIcon);
 video.addEventListener("pause", updateButtonIcon);
 toggle.addEventListener("click", togglePlay);
 skipButtons.forEach(button => button.addEventListener("click" , skip));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range => range.addEventListener("mousemove", handleRangeUpdate));
