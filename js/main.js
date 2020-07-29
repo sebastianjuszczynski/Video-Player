8f99aec57cbe4f04ed3d7c1898bd2cb4f5d438e4
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-07-29 20:55:51 +0100

    function togglePlay added

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelector("[data-skip");
 const ranges = player.querySelector(".player__slider");
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+};
+
+video.addEventListener("click" , togglePlay);
+toggle.addEventListener("click" , togglePlay);
